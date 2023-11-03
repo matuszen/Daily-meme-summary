3db153231d6c8f7d4ec33a7966dc65433f1540cc
matuszen <mateuszn641@gmail.com>
2023-11-03 11:33:17 +0100

    add docstring to utils functions

--- utils.py
+++ utils.py
@@ -2,6 +2,13 @@ import psutil
 
 
 def get_network_usage() -> tuple[int, int]:
+    """Return current number of bytes sent & recived
+
+    Returns
+    -------
+    Tuple of two inteagers
+        (number of bytes sent, number of bytes recived)"""
+
     net_io = psutil.net_io_counters()
 
     return net_io.bytes_sent, net_io.bytes_recv
